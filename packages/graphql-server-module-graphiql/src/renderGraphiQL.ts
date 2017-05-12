ed27df6cb17ff6b72bb64f6a4196f2bf98229ad3
Fei He <fei.he@houzz.com>
2017-05-12 16:29:33 -0700

    retarget graphiql script to customized version

--- packages/graphql-server-module-graphiql/src/renderGraphiQL.ts
+++ packages/graphql-server-module-graphiql/src/renderGraphiQL.ts
@@ -64,11 +64,11 @@ export function renderGraphiQL(data: GraphiQLData): string {
       width: 100%;
     }
   </style>
-  <link href="//cdn.jsdelivr.net/graphiql/${GRAPHIQL_VERSION}/graphiql.css" rel="stylesheet" />
+  <link href="/j/graphiql?file=graphiql.css" rel="stylesheet" />
   <script src="//cdn.jsdelivr.net/fetch/0.9.0/fetch.min.js"></script>
   <script src="//cdn.jsdelivr.net/react/15.0.0/react.min.js"></script>
   <script src="//cdn.jsdelivr.net/react/15.0.0/react-dom.min.js"></script>
-  <script src="//cdn.jsdelivr.net/graphiql/${GRAPHIQL_VERSION}/graphiql.min.js"></script>
+  <script src="/j/graphiql?file=graphiql.min.js"></script>
   ${usingSubscriptions ?
     '<script src="//unpkg.com/subscriptions-transport-ws@0.5.4/browser/client.js"></script>' +
     '<script src="//unpkg.com/graphiql-subscriptions-fetcher@0.0.2/browser/client.js"></script>'
